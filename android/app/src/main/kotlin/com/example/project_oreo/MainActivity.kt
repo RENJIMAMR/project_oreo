a4f59e16870e5770df3fba73e23d3b072f10a959
RENJIMAMR <renjima24@gmail.com>
2024-09-18 17:23:56 +0530

    stage1

---
+++ android/app/src/main/kotlin/com/example/project_oreo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.project_oreo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/utils/constants/color_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorConstants {
+  static Color whiteMain = Colors.white;
+  static Color blackMain = Colors.black;
+  static Color greyMain = Colors.grey;
+  static Color blueMain = Colors.blue;
+  static Color redMain = Colors.red;
+}
